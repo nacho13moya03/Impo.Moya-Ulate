a3278310c4a596b908136c7f59a1e63c6d08d20f
EmersonJPJ <emerson04vade@gmail.com>
2024-02-10 19:03:52 -0600

    Logs exitosos carrito
    Implementación de los logs de éxito en el controlador de carrito

--- API/APIProyectoSC-601/Controllers/CarritoController.cs
+++ API/APIProyectoSC-601/Controllers/CarritoController.cs
@@ -10,14 +10,21 @@ using System.Web.Http;
 
 namespace APIProyectoSC_601.Controllers
 {
+
     public class CarritoController : ApiController
     {
         private readonly Errores log;
+        private readonly LogExitos logExitos;
+
 
         public CarritoController()
         {
-            string rutaDeLogs = ConfigurationManager.AppSettings["RutaDeLogs"];
-            log = new Errores(rutaDeLogs);
+            string rutaErrores = ConfigurationManager.AppSettings["RutaErrores"];
+            string rutaExitos = ConfigurationManager.AppSettings["RutaExitos"];
+
+
+            log = new Errores(rutaErrores);
+            logExitos = new LogExitos(rutaExitos);
         }
 
         [HttpPost]
@@ -37,11 +44,13 @@ namespace APIProyectoSC_601.Controllers
                     {
                         context.Carrito.Add(carrito);
                         context.SaveChanges();
+                        logExitos.Add("RegistrarCarrito", "Registro en el carrito realizado exitosamente");
                     }
                     else
                     {
                         datos.Cantidad = carrito.Cantidad;
                         context.SaveChanges();
+                        logExitos.Add("RegistrarCarrito", "Actualización en el carrito realizada exitosamente");
                     }
                     return "OK";
                 }
@@ -63,23 +72,28 @@ namespace APIProyectoSC_601.Controllers
                 using (var context = new ImportadoraMoyaUlateEntities())
                 {
                     context.Configuration.LazyLoadingEnabled = false;
-                    return (from x in context.Carrito
-                            join y in context.Producto on x.ID_Producto equals y.ID_Producto
-                            where x.ID_Usuario == q
-                            select new
-                            {
-                                x.ID_Carrito,
-                                x.ID_Usuario,
-                                x.ID_Producto,
-                                x.Cantidad,
-                                x.FechaCarrito,
-                                y.Nombre,
-                                y.Precio,
-                                SubTotal = (y.Precio * x.Cantidad),
-                                Impuesto = (y.Precio * x.Cantidad) * 0.13M,
-                                Total = (y.Precio * x.Cantidad) + (y.Precio * x.Cantidad) * 0.13M
-                            }).ToList();
+                    var result = (from x in context.Carrito
+                                  join y in context.Producto on x.ID_Producto equals y.ID_Producto
+                                  where x.ID_Usuario == q
+                                  select new
+                                  {
+                                      x.ID_Carrito,
+                                      x.ID_Usuario,
+                                      x.ID_Producto,
+                                      x.Cantidad,
+                                      x.FechaCarrito,
+                                      y.Nombre,
+                                      y.Precio,
+                                      SubTotal = (y.Precio * x.Cantidad),
+                                      Impuesto = (y.Precio * x.Cantidad) * 0.13M,
+                                      Total = (y.Precio * x.Cantidad) + (y.Precio * x.Cantidad) * 0.13M
+                                  }).ToList();
+
+                    logExitos.Add("ConsultarCarrito", $"Consulta de carrito para el usuario {q} realizada exitosamente");
+
+                    return result;
                 }
+
             }
             catch (Exception ex)
             {
@@ -105,6 +119,7 @@ namespace APIProyectoSC_601.Controllers
                     {
                         context.Carrito.Remove(datos);
                         context.SaveChanges();
+                        logExitos.Add("EliminarRegistroCarrito", $"Registro del carrito con ID {q} eliminado exitosamente");
                     }
                 }
             }
@@ -123,7 +138,11 @@ namespace APIProyectoSC_601.Controllers
             {
                 using (var context = new ImportadoraMoyaUlateEntities())
                 {
-                    return context.PagarCarritoSP(carrito.ID_Usuario).FirstOrDefault();
+                    string resultadoPago = context.PagarCarritoSP(carrito.ID_Usuario).FirstOrDefault();
+
+                    logExitos.Add("PagarCarrito", $"Pago del carrito para el usuario con ID {carrito.ID_Usuario} realizado exitosamente");
+
+                    return resultadoPago;
                 }
             }
             catch (Exception ex)
