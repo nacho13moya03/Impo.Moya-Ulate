9d3b0b756613eb90cce2f00200cd05c01b3d0d75
EmersonJPJ <emerson04vade@gmail.com>
2024-02-10 18:26:39 -0600

    Válidaciones finales cédula y logs id

--- API/APIProyectoSC-601/Controllers/Errores.cs
+++ API/APIProyectoSC-601/Controllers/Errores.cs
@@ -1,62 +1,56 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Web;
 
 namespace APIProyectoSC_601.Controllers
 {
     public class Errores
     {
-            private string Ruta = "";
+        private string Ruta = "";
+        private int idCounter; 
 
-
-            public Errores(string Ruta)
-            {
-                this.Ruta = Ruta;
-            }
-
-            public void Add(string sLog)
-            {
-                CreateDirectory();
-                string nombre = GetNameFile();
-                string cadena = "";
-
-                cadena += DateTime.Now + " - " + sLog + Environment.NewLine;
-
-                StreamWriter sw = new StreamWriter(Ruta + "/" + nombre, true);
-                sw.Write(cadena);
-                sw.Close();
-
-            }
-
-            #region HELPER
-            private string GetNameFile()
-            {
-                string nombre = "";
-
-                nombre = "log_" + DateTime.Now.Year + "_" + DateTime.Now.Month + "_" + DateTime.Now.Day + ".txt";
-
-                return nombre;
-            }
-
-            private void CreateDirectory()
-            {
-                try
-                {
-                    if (!Directory.Exists(Ruta))
-                        Directory.CreateDirectory(Ruta);
-
-
-                }
-                catch (DirectoryNotFoundException ex)
-                {
-                    throw new Exception(ex.Message);
-
-                }
-            }
-            #endregion
+        public Errores(string Ruta)
+        {
+            this.Ruta = Ruta;
+            idCounter = 1; 
         }
 
-    
+        public void Add(string sLog)
+        {
+            CreateDirectory();
+            string nombre = GetNameFile();
+            string cadena = "";
+
+            cadena += "ID: " + idCounter + " - " + DateTime.Now + " - " + sLog + Environment.NewLine;
+
+            StreamWriter sw = new StreamWriter(Ruta + "/" + nombre, true);
+            sw.Write(cadena);
+            sw.Close();
+
+            idCounter++; // Incrementar el contador de ID para cada instancia
+        }
+
+        #region HELPER
+        private string GetNameFile()
+        {
+            string nombre = "";
+
+            nombre = "log_" + DateTime.Now.Year + "_" + DateTime.Now.Month + "_" + DateTime.Now.Day + ".txt";
+
+            return nombre;
+        }
+
+        private void CreateDirectory()
+        {
+            try
+            {
+                if (!Directory.Exists(Ruta))
+                    Directory.CreateDirectory(Ruta);
+            }
+            catch (DirectoryNotFoundException ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+        #endregion
+    }
 }
